165d4fcd4f492fe101b0b57b5788acb627d7e595
BHemert <bjornvanhemert@gmail.com>
2022-03-31 11:01:24 +0200

    Update SendingDataCode.ino

--- ArduinoCodeEmbedded/SendingDataCode/SendingDataCode.ino
+++ ArduinoCodeEmbedded/SendingDataCode/SendingDataCode.ino
@@ -8,10 +8,10 @@
 #include "ESP8266WiFi.h"  // Enables the ESP8266 to connect to the local network (via WiFi)
 
 // WiFi
-//const char* ssid = "Tesla IoT";                 // Your personal network SSID
-//const char* wifi_password = "fsL6HgjN"; // Your personal network password
-const char* ssid = "kruishuis";
-const char* wifi_password = "8ucHeBRu";
+const char* ssid = "Tesla IoT";                 // Your personal network SSID
+const char* wifi_password = "fsL6HgjN"; // Your personal network password
+//const char* ssid = "kruishuis";
+//const char* wifi_password = "8ucHeBRu";
 
 // MQTT
 const char* mqtt_server = "77.161.23.64";  // IP of the MQTT broker
